0f36b1e107907894945f50017742b02af89d8a75
Akshay Kumar <akshay@mac.lan>
2025-10-17 13:06:09 -0400

    Added 404 route handling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Handle GitHub Pages SPA redirect
+const redirect = sessionStorage.redirect
+delete sessionStorage.redirect
+if (redirect && redirect !== location.href) {
+  router.replace(redirect.split('/silicomatics-frontend')[1] || '/')
+}
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
